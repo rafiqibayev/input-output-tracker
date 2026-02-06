212b59950eedaea9034040033579a7478c550d37
Rafiq Ibayev <93721619+rafiqibayev@users.noreply.github.com>
2026-02-06 20:11:08 +0400

    Delete input_output-tracker directory

--- input_output-tracker/constants.ts
+++
@@ -1,9 +0,0 @@
-export const STORAGE_KEY_DATA = 'io-tracker-data';
-export const STORAGE_KEY_CONFIG = 'io-tracker-config';
-
-export const TARGET_YEAR = 2026;
-export const START_DATE = new Date(TARGET_YEAR, 0, 1); // Jan 1, 2026
-export const END_DATE = new Date(TARGET_YEAR, 11, 31); // Dec 31, 2026
-
-export const DEFAULT_INPUT_LABEL = "Work 2 hours";
-export const DEFAULT_OUTPUT_LABEL = "Publish Video";
